dd1dccc4db1af9e39a4105db81851bd546006dd0
Simon Kordas <45798030+skordas@users.noreply.github.com>
2024-11-12 11:19:58 -0500

    Create colorschemes.lua

---
+++ lazyvim/lua/config/plugins/colorschemes.lua
@@ -0,0 +1,14 @@
+-- references:
+-- https://www.lazyvim.org/configuration/examples
+
+return {
+    { "ellisonleao/gruvbox.nvim" },
+
+    -- Configure LazyVim to load gruvbox
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "gruvbox",
+        },
+    },
+}
