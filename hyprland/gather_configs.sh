6cefe0e5cade018cc2739f16cee6ab68a1a126b6
skordas <skordas@redhat.com>
2024-12-03 23:10:05 -0500

    adding waybar

--- hyprland/gather_configs.sh
+++ hyprland/gather_configs.sh
@@ -5,3 +5,8 @@ cp ~/.config/hypr/hyprland.conf .
 cp ~/.config/hypr/hyprpaper.conf .
 cp ~/.config/hypr/hyprlock.conf .
 cp ~/.config/hypr/hypridle.conf .
+cp ~/.config/waybar/style.css ../waybar/style.css
+cp ~/.config/waybar/config.jsonc ../waybar/config.jsonc
+
+# TODO
+# setup starship
--- hyprland/get_configs.sh
+++ hyprland/get_configs.sh
@@ -2,8 +2,9 @@
 
 wget https://raw.githubusercontent.com/skordas/themes-and-settings/refs/heads/main/kitty/kitty.conf -o ~/.config/kitty/kitty.conf
 wget https://raw.githubusercontent.com/skordas/themes-and-settings/refs/heads/main/hyprland/hyprland.conf -o ~/.config/hypr/hyprland.conf
+wget https://raw.githubusercontent.com/skordas/themes-and-settings/refs/heads/main/hyprland/hypridle.conf -o ~/.config/hypr/hypridle.conf
+wget https://raw.githubusercontent.com/skordas/themes-and-settings/refs/heads/main/hyprland/hyprlock.conf -o ~/.config/hypr/hyprlock.conf
+wget https://raw.githubusercontent.com/skordas/themes-and-settings/refs/heads/main/hyprland/hyprpaper.conf -o ~/.config/hypr/hyprpaper.conf
 
 # TODO
-# add hyprpaper
-# add hyprlock
-# add hypridle
+# Set up starship
